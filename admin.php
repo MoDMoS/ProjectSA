7a24c8ac03b930ee82fe2ac0cc5ef1b3c455ac77
MoDMoS <koonmos1@gmail.com>
2021-05-14 21:34:14 +0700

    Enter

--- admin.php
+++ admin.php
@@ -28,9 +28,6 @@
                     <div class="button" align="center">
                         <button class="w3-btn w3-blue w3-round"><a href="login.php">Login</a></button>
                     </div>
-                    <div class="button" align="center">
-                        <button class="w3-btn w3-blue w3-round"><a href="login.php">Login</a></button>
-                    </div>
                 </div>
             </div>
          </div>
